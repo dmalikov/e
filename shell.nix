6cbb8bd9c34e5e48cd8bfce67834a2de27aa337e
Dmitry Malikov <malikov.d.y@gmail.com>
2015-10-13 13:42:41 +0000

    Add standalone-haddock to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }: let
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages( ps: with ps; [
-    cabal2nix doctest hasktags hspec-discover stylish-haskell
+    cabal2nix doctest hasktags hspec-discover standalone-haddock stylish-haskell
   ]);
   hdevtools = pkgs.haskell.packages.${compiler}.callPackage ./hdevtools.nix { };
   cabal-install = pkgs.haskell.packages.${compiler}.cabal-install;
@@ -14,6 +14,5 @@ in
       ${pkg.env.shellHook}
       export IN_WHICH_NIX_SHELL=${name}
       cabal configure --enable-tests --package-db=$NIX_GHC_LIBDIR/package.conf.d
-      alias ghcin='ghci -isrc -package-db=$NIX_GHC_LIBDIR/package.conf.d'
     '';
   }
